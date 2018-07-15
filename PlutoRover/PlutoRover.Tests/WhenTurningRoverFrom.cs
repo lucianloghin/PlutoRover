4f0587f896463098d6b0b8c43ec573829080f27f
lucianloghin <lucian.loghin@gmail.com>
2018-07-15 18:17:39 +0300

    Added turn tests for completion. They passed from the beginning

--- PlutoRover/PlutoRover.Tests/WhenTurningRoverFrom.cs
+++ PlutoRover/PlutoRover.Tests/WhenTurningRoverFrom.cs
@@ -32,5 +32,29 @@ namespace PlutoRover.Tests
 
             Assert.AreEqual(Orientation.North, rover.CurrentPosition.Orientation);
         }
+
+        [Test]
+        public void FacingEastWhenTurningLeftThenPositionDoesNotChange()
+        {
+            Position initialPosition = new Position(0, 0, Orientation.East);
+
+            Rover rover = new Rover(initialPosition);
+            rover.Turn(TurnDirection.Left);
+
+            Assert.AreEqual(initialPosition.X, rover.CurrentPosition.X);
+            Assert.AreEqual(initialPosition.Y, rover.CurrentPosition.Y);
+        }
+
+        [Test]
+        public void FacingEastWhenTurningRightThenPositionDoesNotChange()
+        {
+            Position initialPosition = new Position(0, 0, Orientation.East);
+
+            Rover rover = new Rover(initialPosition);
+            rover.Turn(TurnDirection.Righ);
+
+            Assert.AreEqual(initialPosition.X, rover.CurrentPosition.X);
+            Assert.AreEqual(initialPosition.Y, rover.CurrentPosition.Y);
+        }
     }
 }
