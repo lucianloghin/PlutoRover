f0692769b30bc336877b4d70a1acd222175a7ff5
lucianloghin <lucian.loghin@gmail.com>
2018-07-15 17:09:09 +0300

    Created first failed test

---
+++ PlutoRover/PlutoRover.Tests/WhenRunningSomeTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Components.DictionaryAdapter.Xml;
+using NUnit.Framework;
+
+namespace PlutoRover.Tests
+{
+    [TestFixture]
+    public class WhenRunningSomeTests
+    {
+        // Pluto is a grid
+        // rover will have an initial position
+        // F - forward
+        // B - backwards
+        // L - turn left
+        // R - turn right
+
+        [Test]
+        public void DoSomeStuff()
+        {
+            int initialX = 0;
+            int initialY = 0;
+            string orientation = "N";
+            char step = 'F'; 
+
+            Rover rover = new Rover(initialX, initialY, orientation);
+            rover.Move(step);
+
+            Assert.AreEqual(rover.Y, 1);
+        }
+    }
+
+    public class Rover
+    {
+        private int x;
+        private int y;
+        private string orientation;
+
+        public Rover(int x, int y, string orientation)
+        {
+            this.x = x;
+            this.y = y;
+            this.orientation = orientation;
+        }
+
+        public int Y { get; set; }
+
+        public void Move(char step)
+        {
+        }
+    }
+}
